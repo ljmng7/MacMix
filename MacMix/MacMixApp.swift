818eb505115bee772a8611d109631ec05c4b6724
ljmng7 <1432169343@qq.com>
2026-06-29 11:18:32 +0800

    Initial Commit

---
+++ MacMix/MacMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMixApp.swift
+//  MacMix
+//
+//  Created by 李嘉明 on 2026/6/29.
+//
+
+import SwiftUI
+
+@main
+struct MacMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
